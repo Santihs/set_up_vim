ed7865f60a7feee5bcfbe91cca05fa73eef14074
Santiago Jose Huanacu Sanchez <santiago.huanacu.60094@usalesiana.edu.bo>
2022-12-18 00:35:11 -0400

    Config in lvim

---
+++ lvim/lua/santihs/core/options.lua
@@ -0,0 +1,18 @@
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+opt.wrap = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+-- opt.termguicolors =  true
+-- opt.background = "dark"
+-- opt.signcolumn = "yes"
+
+opt.splitright = true
+opt.splitbelow = true
---
+++ lvim/lua/santihs/plugins/lsp/mason.lua
@@ -0,0 +1,12 @@
+local status, mason_null_ls = pcall(require, "mason-null-ls")
+if not status then
+  return
+end
+
+mason_null_ls.setup({
+  ensure_installed = {
+    "prettier",
+    "stylus",
+    "eslint_d"
+  }
+})
